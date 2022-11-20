8a73ff3076183fc915d9c36837001eedc1ddeeb1
NekrasovaOlga <33664274+NekrasovaOlga@users.noreply.github.com>
2022-11-20 22:27:03 +0300

    Search

--- src/store/galery/galeryAction.js
+++ src/store/galery/galeryAction.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
-
 import { ACCESS_KEY, URL_API } from '../../api/const';
 
 export const GALERY_REQUEST = 'GALERY_REQUEST';
 export const GALERY_REQUEST_SUCCESS = 'GALERY_REQUEST_SUCCESS';
+export const GALERY_REQUEST_UPDATE = 'GALERY_REQUEST_UPDATE';
 export const GALERY_REQUEST_ERROR = 'GALERY_REQUEST_ERROR';
+export const GALERY_CHANGE_PAGE = 'GALERY_CHANGE_PAGE';
 
 export const galeryRequest = () => ({
   type: GALERY_REQUEST,
@@ -15,21 +15,68 @@ export const galeryRequestSuccess = (data) => ({
   galery: data,
 });
 
+export const galeryRequestUpdate = (data) => ({
+  type: GALERY_REQUEST_UPDATE,
+  galery: data,
+});
+
+export const galeryChangePage = (page) => ({
+  type: GALERY_CHANGE_PAGE,
+  page,
+});
+
 export const galeryRequestError = (error) => ({
   type: GALERY_REQUEST_ERROR,
   error,
 });
 
-export const galeryRequestAsync = () => (dispatch) => {
-  dispatch(galeryRequest());
+export const galeryRequestAsync = (page) => (dispatch, getState) => {
+  let pageOld = getState().galery.page;
+  if (page) {
+    pageOld = pageOld++;
+    dispatch(galeryChangePage(pageOld));
+  }
+  const token = getState().token.token;
+  const loading = getState().galery.loading;
+  let after = getState().galery.after;
+  if (loading) return;
 
-  axios(`${URL_API}/photos/`, {
+  fetch(`${URL_API}/photos?page=${page ? pageOld : 1}&per_page=30`, {
     headers: {
-      Authorization: `Client-ID ${ACCESS_KEY}`,
+      Authorization: `Client-ID ${ACCESS_KEY} ${token && `Bearer ${token}`}`,
     },
   })
-    .then(({ data }) => {
-      dispatch(galeryRequestSuccess(data));
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      if (after) {
+        dispatch(galeryRequestUpdate(data));
+      } else {
+        dispatch(galeryRequestSuccess(data));
+      }
+    })
+    .catch((err) => {
+      dispatch(galeryRequestError(err.toString()));
+    });
+};
+
+export const galerySearchAsync = (search) => (dispatch, getState) => {
+  const token = getState().token.token;
+  const loading = getState().galery.loading;
+  console.log(search);
+  if (loading) return;
+
+  fetch(`${URL_API}/search/photos?per_page=30&query=${search}`, {
+    headers: {
+      Authorization: `Client-ID ${ACCESS_KEY} ${token && `Bearer ${token}`}`,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      dispatch(galeryRequestSuccess(data.results));
     })
     .catch((err) => {
       dispatch(galeryRequestError(err.toString()));
--- src/store/galery/galeryReducer.js
+++ src/store/galery/galeryReducer.js
@@ -1,13 +1,18 @@
 import {
+  GALERY_CHANGE_PAGE,
   GALERY_REQUEST,
   GALERY_REQUEST_ERROR,
   GALERY_REQUEST_SUCCESS,
+  GALERY_REQUEST_UPDATE,
 } from './galeryAction';
 
 const initialState = {
   galery: [],
   status: '',
   error: '',
+  page: 1,
+  after: false,
+  loading: true,
 };
 
 export const galeryReducer = (state = initialState, action) => {
@@ -17,6 +22,10 @@ export const galeryReducer = (state = initialState, action) => {
         ...state,
         error: '',
         status: 'loading',
+        loading: true,
+        after: false,
+        page: 1,
+        galery: [],
       };
     case GALERY_REQUEST_SUCCESS:
       return {
@@ -24,12 +33,30 @@ export const galeryReducer = (state = initialState, action) => {
         error: '',
         galery: action.galery,
         status: 'loaded',
+        loading: false,
+      };
+    case GALERY_REQUEST_UPDATE:
+      return {
+        ...state,
+        error: '',
+        galery: [...state.galery, ...action.galery],
+        status: 'loaded',
+        loading: false,
+      };
+    case GALERY_CHANGE_PAGE:
+      return {
+        ...state,
+        error: '',
+        page: action.page + 1,
+        after: true,
+        loading: false,
       };
     case GALERY_REQUEST_ERROR:
       return {
         ...state,
         error: action.error,
         status: 'loaded',
+        loading: false,
       };
 
     default:
